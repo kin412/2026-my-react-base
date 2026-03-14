e4a5ba72394315ab7cf76b4f0fb292ac6ca8c37c
kin412 <kiwo412@gmail.com>
2026-03-15 00:58:00 +0900

    useRef

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,11 +5,10 @@ import CounterMain from "./pages/CounterApp/CounterMain";
 function App() {
   return (
     <>
-      {/* 나의 첫 리액트 프로젝트
-      <BasicTestMain /> */}
-
+      나의 첫 리액트 프로젝트
+      <BasicTestMain />
       {/* 현재값 증감 state 실습 */}
-      <CounterMain />
+      {/* <CounterMain /> */}
     </>
   );
 }

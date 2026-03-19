1c43b6c9b4d6e4f0a4ee2399bfa0fb551ee7c958
kin412 <kiwo412@gmail.com>
2026-03-19 18:53:19 +0900

    자체프로젝트 x next.js에서 계속

--- typescript/src/pages/basic/BasicMain.tsx
+++ typescript/src/pages/basic/BasicMain.tsx
@@ -1,5 +1,11 @@
 const BasicMain = () => {
-  return <div>BasicMain</div>;
+  return (
+    <div>
+      <div>BasicMain</div>
+      <div>타입스크립트 자체 프로젝트로는 크게 할게 없는듯함.</div>
+      <div>넥스트로 넘어가시오</div>
+    </div>
+  );
 };
 
 export default BasicMain;
